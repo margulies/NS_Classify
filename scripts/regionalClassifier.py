many fixes and added calculation of feature importances at classify stage. also moved set up of masks into __init__

scripts/regionalClassifier.py
@@ -15,52 +15,62 @@ from sklearn.ensemble import GradientBoostingClassifier
 
 
 class maskClassifier:
-    def __init__(self, dataset, classifier=GradientBoostingClassifier(), thresh = 0.08, 
+    def __init__(self, dataset, masks, classifier=GradientBoostingClassifier(), thresh = 0.08, 
         param_grid={'max_features': np.arange(2, 140, 44), 'n_estimators': np.arange(5, 141, 50),
           'learning_rate': np.arange(0.05, 1, 0.1)}):
 
+        self.masklist = zip(masks, range(0, len(masks)))
+
+        self.mask_num = len(self.masklist)
+
         # Where to store differences, results, n's and dummy results
         self.diffs = {}
-        self.class_score = np.zeros((7, 7))
-        self.ns = np.zeros((7, 7))
-        self.dummy_score = np.zeros((7, 7))
+        self.class_score = np.zeros((self.mask_num, self.mask_num))
+        self.ns = np.zeros((self.mask_num, self.mask_num))
+        self.dummy_score = np.zeros((self.mask_num, self.mask_num))
 
         self.classifier = classifier
         self.dataset = dataset
         self.thresh = thresh
         self.param_grid = param_grid
 
-        self.fit_clfs = np.empty((7, 7), object) # Fitted classifier
-        self.c_data = np.empty((7, 7), tuple)    # Actual data
+        self.fit_clfs = np.empty((self.mask_num, self.mask_num), object) # Fitted classifier
+        self.c_data = np.empty((self.mask_num, self.mask_num), tuple)    # Actual data
 
-    def classify(self, masks):
+    def classify(self, calculate_importances=False):
 
-        masklist = zip(masks, range(0, len(masks)))
-
-        iters = list(itertools.combinations(masklist, 2))
+        iters = list(itertools.combinations(self.masklist, 2))
         prog = 0.0
         total = len(list(iters))
         self.update_progress(0)
 
 
+        if calculate_importances:
+            self.feature_importances = np.empty((self.mask_num, self.mask_num), object)           
+
         for pairs in iters:
 
-            output = classify.classify_regions(self.dataset, [pairs[0][0], pairs[1][0]],
+            index = (pairs[0][1], pairs[1][1])
+            names = [pairs[0][0], pairs[1][0]]
+
+            output = classify.classify_regions(self.dataset, names,
                     classifier=self.classifier, param_grid=self.param_grid,
                     threshold=self.thresh, output='summary_clf')
 
 
-            self.class_score[pairs[0][1], pairs[1][1]] = output['score']
+            self.class_score[index] = output['score']
 
-            self.ns[pairs[0][1], pairs[1][1]] = output['n'][0] + output['n'][1]
+            self.ns[index] = output['n'][0] + output['n'][1]
 
-            self.fit_clfs[pairs[0][1], pairs[1][1]] = output['clf']
+            self.fit_clfs[index] = output['clf']
 
-            self.c_data[pairs[0][1], pairs[1][1]] = classify.get_studies_by_regions(dataset, [pairs[0][0],
-                    pairs[1][0]], threshold=self.thresh)
+            self.c_data[index] = classify.get_studies_by_regions(dataset, names, threshold=self.thresh)
 
-            self.dummy_score[pairs[0][1], pairs[1][1]] = classify.classify_regions(dataset, 
-                [pairs[0][0], pairs[1][0]], method='Dummy', threshold=self.thresh)['score']
+            self.dummy_score[index] = classify.classify_regions(dataset, 
+                names, method='Dummy', threshold=self.thresh)['score']
+
+            if calculate_importances:
+                 self.feature_importances[index] = self.fit_clfs[index].clf.fit(*self.c_data[index]).feature_importances_
 
             prog = prog + 1
             self.update_progress(int(prog / total * 100))
@@ -71,13 +81,14 @@ class maskClassifier:
         self.diffs = self.class_score - self.dummy_score
 
         # Make results fill in across diagonal
-        for j in range(0, 7):
-            for b in range(0, 7):
+        for j in range(0, self.mask_num):
+            for b in range(0, self.mask_num):
                 if self.diffs.mask[j, b]:
                     self.diffs[j, b] = self.diffs[b, j]
                     self.fit_clfs[j, b] = self.fit_clfs[b, j]
                     self.ns[j, b] = self.ns[b, j]
                     self.c_data[j, b] = self.c_data[b, j]
+                    self.feature_importances[j, b] = self.feature_importances[b, j]
 
     def get_mask_averages(self):
         return [self.diffs[k].mean() for k in range(0, self.diffs.shape[0])]
@@ -106,10 +117,13 @@ class maskClassifier:
     def get_importances(self, index, sort=True, relative=True):
         """ get the importances with feature names given a tuple mask index """
 
-        if self.param_grid:
+        if self.feature_importances[index]:
+            fi = self.feature_importances[index]
+        elif self.param_grid:
             fi = self.fit_clfs[index].clf.best_estimator_.feature_importances_
         else:
-            fi = self.fit_clfs[index].clf.fit(self.c_data[index][0], yeoClass.c_data[index][1]).feature_importances_
+            fi = self.fit_clfs[index].clf.fit(self.c_data[index][0], 
+                yeoClass.c_data[index][1]).feature_importances_
 
         if relative:
         	fi = 100.0 * (fi / fi.max())
@@ -132,7 +146,7 @@ class maskClassifier:
     	imps = imps[imps>thresh]
 
     	names = np.array(names)
-    	names[:len(imps)]
+    	names = names[-len(imps):]
 
     	sorted_idx = np.argsort(imps)
     	pos = np.arange(sorted_idx.shape[0]) + .5
@@ -148,7 +162,7 @@ class maskClassifier:
         sys.stdout.write('\r[{0}] {1}%'.format('#' * (progress / 10),
                          progress))
 
-dataset = Dataset.load('../data/pickled.pkl')
+dataset = Dataset.load('../data/dataset.pkl')
 
 masks = ['7Networks_Liberal_1.nii.gz', '7Networks_Liberal_2.nii.gz',
             '7Networks_Liberal_3.nii.gz', '7Networks_Liberal_4.nii.gz',
@@ -158,3 +172,4 @@ masks = ['7Networks_Liberal_1.nii.gz', '7Networks_Liberal_2.nii.gz',
 rootdir = '../masks/Yeo_JNeurophysiol11_MNI152/standardized/'
 
 masklist = [rootdir + m for m in masks]
+